f57f3e6c02863da3df0671114a854e553b953ee6
Yasser EL-Nagar <yasser.elnagar.2021@gmail.com>
2018-07-08 18:23:59 +0200

    added facebook login code but there is bug in it
    added facebook login code but there is bug in it  that will be fixed tomorrow

--- CheaperApp/app/src/main/java/com/example/unknown/cheaperapp/MainActivity.java
+++ CheaperApp/app/src/main/java/com/example/unknown/cheaperapp/MainActivity.java
@@ -3,15 +3,23 @@ package com.example.unknown.cheaperapp;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -28,10 +36,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getKeyHash();
+
         handleNavigationClicks();
 
         navigationView.getMenu().clear();
         navigationView.inflateMenu(R.menu.drawable_nav_items_logout);
+
+
 
     }
 
@@ -79,6 +91,25 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
 
         return true;
+    }
+
+    private void  getKeyHash(){
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    getPackageName(),
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        }
+        catch (PackageManager.NameNotFoundException e) {
+
+        }
+        catch (NoSuchAlgorithmException e) {
+
+        }
     }
 
 
