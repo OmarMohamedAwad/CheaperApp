82c1b4de341d851b0dc701390971b94b0bcb5412
ahmedfetoh1997 <39446636+ahmedfetoh1997@users.noreply.github.com>
2018-07-11 16:50:45 +0200

    EditBranch,DeleteBranch
    EditBranch,DeleteBranch
    no label in mainfest =" "

---
+++ CheaperApp/app/src/main/java/com/example/unknown/cheaperapp/EditBranch_Activity.java
@@ -0,0 +1,13 @@
+package com.example.unknown.cheaperapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditBranch_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_branch_);
+    }
+}
